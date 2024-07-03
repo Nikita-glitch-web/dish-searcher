1572a22a7ba59921170ea97725259f0e98cc8801
Nick <nikitalytvynov0506@gmail.com>
2024-07-03 12:25:22 +0300

    last-commit

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,50 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import style from "./HomePage.module.css";
+import { SearchForm } from "../SearchForm/SearchForm";
 
 const HomePage = () => {
-    // const mealRequest = () => {
-    //   return `www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
-    // };
-    // mealRequest();
+    const navigate = useNavigate();
+  // const mealRequest = () => {
+  //   return `www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`;
+  // };
+  // mealRequest();
 
-  const [inputValue, setInputValue] = useState("");
-
-  const SubmitInputChange = (e) => {
-    setInputValue(e.target.value);
-    // JSON.stringify(inputValue);
-    console.log("input submit works");
-    JSON.stringify(inputValue);
-        fetch(
-          `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`,
-          {
-            method: "GET",
-          }
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .then((responseData) => {
-            console.log(responseData);
-          });
+  const submitHandler = (value) => {
+    console.log(value);
+    navigate(`/search?q=${value}`);
   };
 
-//   const SubmitFormChange = () => {
-//     console.log(
-//         "Form is submitted"
-//     )
-//   }
+  //   const SubmitFormChange = () => {
+  //     console.log(
+  //         "Form is submitted"
+  //     )
+  //   }
 
   return (
     <div className={style.search_bar_container}>
-      <form >
-        <input
-          value={inputValue}
-          type="text"
-          placeholder="Enter ingredient"
-          className={style.search_bar_input}
-          onChange={SubmitInputChange}
-        />
-      </form>
+      <SearchForm onSubmit={submitHandler} />
     </div>
   );
 };
--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { SearchForm } from "../SearchForm/SearchForm";
 import style from "./SearchPage.module.css";
 
 export const SearchPage = (index) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState([]);
+  console.log(items);
+  const makeSearchRequest = async (inputValue) => {
+    try {
+      const request = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`,
+        {
+          method: "GET",
+        }
+      );
+      const data = await request.json();
+      console.log(data);
+      setItems(data.meals);
+    } catch (e) {
+      console.error(e);
+      alert("ERRRO");
+    }
+  };
+
+  useEffect(() => {
+    console.log("RENDERD", searchParams.get("q"));
+    const queryParamsValue = searchParams.get("q");
+    if (queryParamsValue) {
+      setInputValue(queryParamsValue);
+      makeSearchRequest(queryParamsValue);
+    }
+  }, [searchParams]);
+
+  const submitHandler = (value) => {
+    console.log(value);
+    setSearchParams({ q: value });
+  };
+
   return (
     <div className={style.search_bar_container}>
-      <input
-        placeholder="Enter ingredient"
-        className={style.search_bar_input}
-      />
+      <SearchForm defaultValue={inputValue} onSubmit={submitHandler} />
+      {items.map((item) => {
+        console.log(item)
+        return (
+          <div className={style.details_content_greed}>
+            <Link to={`/details/${item.idMeal}`} title={item.idMeal}>
+              <div className={style.details_content_item}>
+                <img
+                  alt=""
+                  src={item.strMealThumb}
+                  className={style.meal_img}
+                />
+                <h1>{item.strMeal}</h1>
+                <p>{item.strInstructions}</p>
+                <div className={style.ingredients_column}>
+                </div>
+              </div>
+            </Link>
+            </div>
+        );
+      })}
     </div>
   );
 };
-
-
